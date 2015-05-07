50e150dbc7055de3ae66f960201d0561a575b158
Asger Behncke Jacobsen <asger@8kilo.com>
2015-05-07 18:24:41 +0200

    update

--- test/extensis_portfolio/asset_downloader_test.rb
+++ test/extensis_portfolio/asset_downloader_test.rb
@@ -7,7 +7,7 @@ module ExtensisPortfolio
     let(:username) { "kamila.farshchi" }
     let(:password) { "Mh-kl#Q4TRMm&S8a" }
     let(:connection) { ExtensisPortfolio::Connection.new(server, username, password) }
-    let(:asset_id) { "2021" }
+    let(:asset_id) { "2018" }
     let(:catalog_id) { "87C27791-4420-469D-0FD8-611E790AEDFA" }
 
     subject { ExtensisPortfolio::AssetDownloader.new(connection, catalog_id) }
